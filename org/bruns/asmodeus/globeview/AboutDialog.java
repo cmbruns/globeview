2bc5be26da2cf1d7ad7e5beeb95c5e8810b38e89
cmbruns <cmbruns@c3c21c00-0202-0410-ac1a-b293e0832186>
2005-03-11 00:00:00 +0000

    Now descends from InfoDialog class New credit for city data Pointer to bugzilla site
    git-svn-id: https://simtk.org/svn/globeview/trunk@34 c3c21c00-0202-0410-ac1a-b293e0832186

--- org/bruns/asmodeus/globeview/AboutDialog.java
+++ org/bruns/asmodeus/globeview/AboutDialog.java
@@ -10,6 +10,11 @@
 // $Id$
 // $Header$
 // $Log$
+// Revision 1.5  2005/03/11 00:00:00  cmbruns
+// Now descends from InfoDialog class
+// New credit for city data
+// Pointer to bugzilla site
+//
 // Revision 1.4  2005/03/04 23:54:30  cmbruns
 // Added acknowledgements for image and coast data
 //
@@ -24,87 +29,33 @@
 package org.bruns.asmodeus.globeview;
 
 import org.bruns.asmodeus.globeview.*;
-import java.util.*;
 import java.awt.*;
-import java.awt.event.*;
-import java.text.*;
 
-public class AboutDialog extends Dialog {
+public class AboutDialog extends InfoDialog {
 	AboutDialog(Frame frame) {
 		super(frame, "About GlobeView", true); // modal dialog
+
+		addLine("Globevew version 1.10 by Chris Bruns");
+		addLine("Copyright \u00A9 2001-2005 all rights reserved");
+		addLine("http://bruns.homeip.net/~bruns/globeview.html");
+		addLine("cmbruns@comcast.net");
+		addLine(" ");		
+		addLine("Submit feature requests and bug reports to:");
+		addLine("  http://bruns.homeip.net/bugzilla/index.cgi");
+		addLine("");
+		addLine("Satellite images from NASA courtesy of:");		
+		addLine("  Reto St\u00F6ckli, NASA Earth Observatory");
+		addLine("  rstockli@climate.gsfc.nasa.gov");
+		addLine("  http://earthobservatory.nasa.gov/Newsroom/BlueMarble/");
+		addLine(" ");		
+		addLine("City data courtesy of:");		
+		addLine("  Stefan Helders, World Gazetteer");
+		addLine("  http://www.world-gazetteer.com/");
+		addLine(" ");		
+		addLine("Coast line data courtesy of:");		
+		addLine("  United States Central Intelligence Agency (CIA)");
+		addLine("  World Databank II");
 		
-		GridBagLayout gridbag = new GridBagLayout();
-		GridBagConstraints constraints = new GridBagConstraints();
-		
-		//Create OK button
-		Button okButton = new Button( "OK" );
-		okButton.addActionListener ( new ActionListener() 
-									 {
-			public void actionPerformed( ActionEvent e )
-		{
-				// Hide dialog
-				setVisible(false);
-		}
-									 });
-		
-		
-		// Create message to display
-		Panel textPanel = new Panel();
-		textPanel.setLayout(new GridLayout(0,1,0,0)); // single column
-		textPanel.add(new Label("Globevew version 1.10 by Chris Bruns"));
-		textPanel.add(new Label("Copyright 2001-2005 all rights reserved"));
-		textPanel.add(new Label("http://bruns.homeip.net/~bruns/globeview.html"));
-		textPanel.add(new Label("cmbruns@comcast.net"));
-		textPanel.add(new Label(" "));		
-		textPanel.add(new Label("Satellite images from NASA courtesy of:"));		
-		textPanel.add(new Label("  Reto Stoeckli, NASA Earth Observatory"));
-		textPanel.add(new Label("  rstockli@climate.gsfc.nasa.gov"));
-		textPanel.add(new Label("  http://earthobservatory.nasa.gov/Newsroom/BlueMarble/"));
-		textPanel.add(new Label(" "));		
-		textPanel.add(new Label("Coast line data courtesy of:"));		
-		textPanel.add(new Label("  United States Central Intelligence Agency (CIA)"));
-		textPanel.add(new Label("  World Databank II"));
-		
-		// TODO - find a way to print out build time
-		// SimpleDateFormat dateFormat = new SimpleDateFormat();
-		// String dateString = dateFormat.format(new Date());
-		// textPanel.add(new Label("Build: " + dateString));
-		
-		setLayout( gridbag );
-		
-		// The message should not fill, it should be centered within
-		// this area, with
-		// some extra padding.  The gridwidth of REMAINDER means this
-		// is the only
-		// thing on its row, and the gridheight of RELATIVE means
-		// there should only
-		// be one thing below it.
-		constraints.fill = GridBagConstraints.NONE;
-		constraints.anchor = GridBagConstraints.CENTER;
-		constraints.ipadx = 20;
-		constraints.ipady = 20;
-		constraints.weightx = 1.0;
-		constraints.weighty = 1.0;
-		constraints.gridwidth = GridBagConstraints.REMAINDER;
-		constraints.gridheight = GridBagConstraints.RELATIVE;
-		
-		gridbag.setConstraints(textPanel, constraints);
-		add(textPanel);
-		
-		// The button has no padding, no weight, taked up minimal width, and
-		// Is the last thing in its column.
-		constraints.ipadx = 0;
-		constraints.ipady = 0;
-		constraints.weightx = 0.0;
-		constraints.weighty = 0.0;
-		constraints.gridwidth = 1;
-		constraints.gridheight = GridBagConstraints.REMAINDER;
-		
-		gridbag.setConstraints( okButton, constraints );
-		
-		add( okButton );
-		
-		pack();
-		setResizable(false);
+		finalizeDialog();
 	}
 }
