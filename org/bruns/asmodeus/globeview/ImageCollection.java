9b9acee84037fab1075a6904644b10854ddb86dc
cmbruns <cmbruns@c3c21c00-0202-0410-ac1a-b293e0832186>
2005-03-13 22:06:03 +0000

    Changes to keep track of which images are most recently used, and to delete the stalest ones. Changes to blur the pixel boundaries at hyper zoomed zoom levels.
    git-svn-id: https://simtk.org/svn/globeview/trunk@57 c3c21c00-0202-0410-ac1a-b293e0832186

--- org/bruns/asmodeus/globeview/ImageCollection.java
+++ org/bruns/asmodeus/globeview/ImageCollection.java
@@ -8,6 +8,10 @@
 //  $Id$
 //  $Header$
 //  $Log$
+//  Revision 1.3  2005/03/13 22:06:03  cmbruns
+//  Changes to keep track of which images are most recently used, and to delete the stalest ones.
+//  Changes to blur the pixel boundaries at hyper zoomed zoom levels.
+//
 //  Revision 1.2  2005/03/11 00:10:30  cmbruns
 //  Added printline about image count for debugging memory
 //  Removed reference to MapBlitter.rawImage in check
@@ -37,6 +41,77 @@ public class ImageCollection extends GeoCollection {
 		canvas = geoCanvas;
 	}
 	
+	// Delete some of the image information to conserve memory
+	int maxImagesToKeep = 15;  // Delete images if there are more than this
+	int minImagesToKeep = 5; // Do not delete images if there are less than this
+	void flushSomeImages() {
+		// consider destroying some images
+
+		Vector sortedImages = sortImagesByTime(); // Oldest images are first
+		int activeImageCount = sortedImages.size();
+		// System.out.println(""+activeImageCount+" images with data loaded");
+		// if (activeImageCount <= minImagesToKeep) return;
+
+		long currentTime = (new Date()).getTime();
+		for (int i = 0; i < sortedImages.size(); i++) {
+			MapBlitter image = (MapBlitter) sortedImages.elementAt(i);
+			double secondsSinceUse = (currentTime - image.timeOfPreviousUse) / 1000.0;
+			// System.out.println("  Time of last use = "+secondsSinceUse);
+			// if ((sortedImages.size() - i) <= minImagesToKeep) return; // Remaining images are keepers
+			if ((sortedImages.size() - i) > maxImagesToKeep) { // Over limit - definitely remove
+				image.pixelArray = null;
+				continue;
+			}
+			// More rules for intermediate images that are on probation
+			if (secondsSinceUse > 1200) { // Unused for 20 minutes
+				image.pixelArray = null;
+				continue;
+			}
+		}
+	}
+
+	// Return non-null images sorted by time of last use - oldest first
+	Vector sortImagesByTime() {
+		Vector images = new Vector();
+		for (int i = 0; i < subObject.size(); i++) {
+			MapBlitter image = (MapBlitter) subObject.elementAt(i);
+			if (image.pixelArray != null) {
+				images.addElement(image);
+			}
+		}
+		return quickSortImages(images);
+	}
+	
+	Vector quickSortImages(Vector images) {
+		if (images.size() <= 1) return images;
+
+		Vector greaterVector = new Vector();
+		Vector equalVector = new Vector();
+		Vector lessVector = new Vector();
+
+		// TODO choose pivot more randomly
+		MapBlitter pivot = (MapBlitter) images.elementAt(0);
+
+		for (int i = 0; i < images.size(); i++) {
+			MapBlitter image = (MapBlitter) images.elementAt(i);
+			if (image.timeOfPreviousUse == pivot.timeOfPreviousUse)
+				equalVector.addElement(image);
+			else if (image.timeOfPreviousUse > pivot.timeOfPreviousUse)
+				greaterVector.addElement(image);
+			else // (image.timeOfPreviousUse < pivot.timeOfPreviousUse)
+				lessVector.addElement(image);
+		}
+		
+		// sort subVectors
+		lessVector = quickSortImages(lessVector);
+		greaterVector = quickSortImages(greaterVector);
+		
+		// Combine into one vector
+		lessVector.addAll(equalVector);
+		lessVector.addAll(greaterVector);
+		return lessVector;
+	}
+	
     // Paint the texture map, simultaneously to two destinations
     // offScreenGraphics is the off-screen image
     // onScreenGraphics is the on-screen image
@@ -46,8 +121,9 @@ public class ImageCollection extends GeoCollection {
     //  No function calls
     int stripWidth = 10; // How often to update progress on-screen
     Vector3D sunVector;
-	void paint(Graphics onScreenGraphics, GenGlobe genGlobe, Projection projection, LensRegion viewLens) {
-		System.out.println("" + elementCount() + " images loaded");
+	void paint(Graphics onScreenGraphics, GenGlobe genGlobe, Projection projection, LensRegion viewLens) 
+	{
+		// System.out.println("" + elementCount() + " images loaded");
 		
 		// This is cheating, not using arguments to pass this Graphics, but I don't want to
 		// change the paint signature for all GeoObjects
@@ -76,17 +152,17 @@ IMAGES:
 			// Date t1 = new Date();
 			// sunVector = getSunVector2();
 			Date currentDate = new Date();
-			try {
+			// try {
 				SimpleDateFormat dateFormat = 
 				new SimpleDateFormat("dd-MMM-yyyy hh:mm:ss a z");
 				// currentDate = dateFormat.parse("01-Jun-2008 12:00:00 PM GMT");
 				// currentDate = dateFormat.parse("09-May-2003 07:05:02 PM PST");	  
-			} catch (Exception e) {}
+			// } catch (Exception e) {}
 			sunVector = Sun.getVector(currentDate);
 		}
 		
-		Vector3D tempv1 = new Vector3D(); // These two are NEVER null
-		Vector3D v1 = tempv1; // These may be null
+		Vector3D tempv1 = new Vector3D(); // This is NEVER null
+		Vector3D v1 = tempv1; // This may be null
 		
 		Color color;
 		int pixel, darkPixel, shadePixel;
@@ -114,9 +190,16 @@ IMAGES:
 		double alpha; // Day/night ratio for twilight zone
 		double fakeAngle;
 		
+		// keep track of which Bitmaps are actually used
+		boolean[] usedBitmaps = new boolean[usableBitmaps.size()]; // initializes to false
 		
+		// For computing blur on hyper zoomed images
+		boolean previousPlot = false; // Was the pixel above drawn from bitmap?
+		Vector3D previousSpot = new Vector3D();
+
 		for (x = 0; x < xMax; ++x) {
 			cx = (x - genGlobe.centerX) * rInv;
+			previousPlot = false;
 			for (y = 0; y < yMax; ++y) {
 				mapIndex = x + y * width;
 				
@@ -125,23 +208,42 @@ IMAGES:
 				cy = (genGlobe.centerY - y) * rInv;
 				
 				v1 = projection.vec2DTo3D(cx, cy, tempv1);
-				if (v1 == null) continue; // Clipping (function failed)
+				if (v1 == null) {
+					previousPlot = false;
+					continue; // Clipping (function failed)
+				}
 				
 				// Oblique transform (centers globe on point of interest)
 				v1 = genGlobe.unrotate(v1);
+
+				// Might want to blur hyper zoomed pixels
+				// Approximate angular distance between pixels
+				double pixelSize = 0;
+				if (previousPlot) pixelSize = (v1.minus(previousSpot)).length();
+				previousSpot.copy(v1);
 				
 				pixel = 0;				
 				// Look for pixel in each bitmap, starting with highest resolution
 IMAGES:
 				for (int i = usableBitmaps.size() - 1; i >= 0; i--) {
 					MapBlitter mapBlitter = (MapBlitter) usableBitmaps.elementAt(i);
-					pixel = mapBlitter.getPixel(v1, 0);
+					try {
+						pixel = mapBlitter.getPixel(v1, pixelSize);
+					} catch (InterruptedException e) {
+						return;
+					}
 					if (pixel != 0) {
+						usedBitmaps[i] = true;
 						break IMAGES;
 					}
 				}
 				
-				if (pixel == 0) pixel = canvas.oceanColorInt;
+				if (pixel == 0) {
+					pixel = canvas.oceanColorInt;
+					previousPlot = false;
+				}
+				else
+					previousPlot = true;
 				
 				// Check for day/night
 				if (canvas.dayNight) {
@@ -197,11 +299,11 @@ IMAGES:
 				}
 				
 				canvas.offScreenPixels[mapIndex] = pixel;
-				if (!keepDrawing) {canvas.unsetWait(); return;} // for very fast abortion
+				if (!keepDrawing) {return;} // for very fast abortion
 
 				}
 			
-			if (!keepDrawing) {canvas.unsetWait(); return;}
+			if (!keepDrawing) {return;}
 			
 			// When bitmap is slow, update on-screen image
 			// if (((x % stripWidth) == 0) && (x > 0)){
@@ -213,5 +315,14 @@ IMAGES:
 			// }
 		}
 		// canvas.offScreenSource.newPixels(0,0,width,height);
-	}	
+		
+		// Update time stamp on those bitmaps which were used in this draw
+		long timeStamp = (new Date()).getTime();
+		for (int i = 0; i < usableBitmaps.size(); i++) {
+			if (usedBitmaps[i]) {
+				MapBlitter mapBlitter = (MapBlitter) usableBitmaps.elementAt(i);
+				mapBlitter.timeOfPreviousUse = timeStamp;
+			}
+		}
+	}
 }
